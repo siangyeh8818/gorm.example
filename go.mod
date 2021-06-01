6e6e2c328c50e3d610937ed21b89ddc65b9c478b
Jia-siang Ye <siangyeh8818@gmail.com>
2021-06-01 23:59:16 +0800

    feat: add sqllite-example code

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/siangyeh8818/gorm.example
+
+go 1.14
+
+require (
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.21.10
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	gormexample "github.com/siangyeh8818/gorm.example/internal"
+)
+
+func main() {
+
+	gormexample.SqlliteExample()
+}
